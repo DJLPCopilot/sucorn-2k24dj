ad672f409345c3245301fb8ddc6b1eebd4dc6a96
djlptts <125202938+djlptts@users.noreply.github.com>
2024-12-26 19:02:47 -0500

    OK

--- .history/utilities/imagen3/imagen3_20241226190242.py
+++ .history/utilities/imagen3/imagen3_20241226190242.py
@@ -5,7 +5,7 @@ single image case, hard to see for those more than 1
 {
     "imagePanels": [
         {
-        "prompt":"2-D anime, Blonde wife and husband with light skin and black hair riding on giant hopper balls in a trampoline room",
+        "prompt":"2-D anime, Blonde  wife and husband with light skin and black hair riding on giant hopper balls in a trampoline room",
         "generatedImages": [
             {
             "encodedImage": "",
